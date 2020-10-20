a44d7d95af064a92b41c71f72b2150fa54b62a01
Obwuda <73200816+Obwuda@users.noreply.github.com>
2020-10-20 18:34:47 -0400

    why must modals do this

--- src/popups.js
+++ src/popups.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Dropdown } from "semantic-ui-react";
 // import { chars, char_info, mat_gems } from "./genshin_data_english";
 // import { char_imgs } from "./image_srcs_data";
 // import CharInfo from "./char_resource.js";
-// import Modal from "react-modal";
-import { Modal, ModalWrapper } from "carbon-components-react";
-// import { Container, Row, Col, Button } from "reactstrap";
+import Modal from "react-modal";
+import { Container, Row, Col, Button } from "reactstrap";
 
-class InfoModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { sel_char: null, sel_asc: null, is_open: false };
-  }
+function Example() {
+  const [show, setShow] = useState(false);
 
-  render() {
-    return <div></div>;
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
-export default InfoModal;
+export default Example;
